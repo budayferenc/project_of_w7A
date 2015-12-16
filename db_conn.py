dd0ac8ae692bcb7e2186b71ced2af1e0803e8747
Ferenc Buday <budayferenc@gmail.com>
2015-12-16 14:47:40 +0100

    none db_conn.py

--- db_conn.py
+++
@@ -1,7 +0,0 @@
-"""import mysql.connector
-
-cnx = mysql.connector.connect(user='root', password='localhost',
-                              host='127.0.0.1',
-                              database='meetupsdb')
-cnx.close()"""
-print("haha")
